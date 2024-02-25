2ff3171867c5233a39cecec39a28a570ab0d4ff4
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2024-02-25 11:24:21 +0100

    improved example

--- example/script.js
+++ example/script.js
@@ -1,31 +1,39 @@
 import sectionResizer from "./section-resizer.js";
 
+// setup the main content as an horizontal section-resizer
 const container = document.getElementById("content");
-const container_left = document.getElementById("content-left");
-const container_right = document.getElementById("content-right");
-const container_right_top = document.getElementById("right-top");
+const srMain = sectionResizer(container);
+srMain.resize([{ index: 0, size: 200 }]); // make first section initial size 200px
 
-const minSize = 100;
+// setup the left container to be a vertical section-resizer
+const containerLeft = document.getElementById("content-left");
+const srLeft = sectionResizer(containerLeft, { mode: "vertical" });
+srLeft.configure({ min: 200 }); // make top section of left panel minimum size 200px
 
-const srMain = sectionResizer(container, { resizeMode: "right" });
+// ... same for right container
+const containerRight = document.getElementById("content-right");
+const srRight = sectionResizer(containerRight, { mode: "vertical" });
+srRight.configure([{ index: 1, min: 300, max: 300 }]); // configure second section to be fixed
 
-const srLeft = sectionResizer(container_left, { mode: "vertical" });
+// setup the top right container to be an horizontal section-resizer
+const containerRightTop = document.getElementById("right-top");
+const srRightTop = sectionResizer(containerRightTop);
 
-const srRight = sectionResizer(container_right, { mode: "vertical" });
-
-const srRightTop = sectionResizer(container_right_top);
-
-srMain.resize([{ index: 0, size: 200 }]);
-srLeft.configure({ min: 200 });
-srRight.configure([{ index: 1, min: 300, max: 300 }]);
-srRightTop.configure({ min: minSize });
-srMain.configure([
-  { index: 1, min: minSize * srRightTop.getSections().length },
-]);
-
-console.log(srRightTop.getSections());
-
-const btn = document.getElementById("add-btn");
-btn.addEventListener("click", () => {
-  console.log(click);
+// configure + and - buttons to showcase dynamic support of section-resizer
+const btnPlus = document.getElementById("add-btn");
+btnPlus.addEventListener("click", () => {
+  const n = srRightTop.getSections().length;
+  if (n < 10) {
+    const div = document.createElement("div");
+    div.classList.add("child");
+    div.textContent = `div #${n + 1}`;
+    containerRightTop.appendChild(div);
+  }
+});
+const btnMinus = document.getElementById("rem-btn");
+btnMinus.addEventListener("click", () => {
+  const elements = srRightTop.getSections();
+  if (elements.length > 1) {
+    containerRightTop.removeChild(elements[elements.length - 1].element);
+  }
 });
